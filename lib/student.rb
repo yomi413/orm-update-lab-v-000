43db9869f287ae9ba34b93a84acdd529696d25b4
Yomaira Escano <yjescano@gmail.com>
2017-12-26 05:04:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,11 +52,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    # new_student = self.new
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
   # def self.find_by_name(name)
