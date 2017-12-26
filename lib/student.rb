5078de9c7524c81f4ba3b3451f9a3bae9bc65808
Yomaira Escano <yjescano@gmail.com>
2017-12-26 04:03:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
