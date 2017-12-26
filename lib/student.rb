9f2abcbddf69896d61003b01c01bd18bd6636521
Yomaira Escano <yjescano@gmail.com>
2017-12-26 04:49:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,17 +59,17 @@ class Student
     new_student
   end
 
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-    SQL
-
-    DB[:conn].execute(sql, name).collect do |row|
-      self.new_from_db(row)
-    end
-  end
+  # def self.find_by_name(name)
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM students
+  #     WHERE name = ?
+  #   SQL
+  #
+  #   DB[:conn].execute(sql, name).collect do |row|
+  #     self.new_from_db(row)
+  #   end
+  # end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
