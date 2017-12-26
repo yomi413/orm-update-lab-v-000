1de59f0c13b4bd264ce0aa5cfaa5b7912351f83e
Yomaira Escano <yjescano@gmail.com>
2017-12-26 04:36:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,12 @@ class Student
     student
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   # Remember, you can access your database connection anywhere in this class
